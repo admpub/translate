a87b3fc63eda9ac91cb9514b05bc04af0ebe759c
Hank Shen <swhbox@foxmail.com>
2025-12-10 17:14:42 +0800

    update

--- providers/init.go
+++ providers/init.go
@@ -2,7 +2,7 @@ package providers
 
 import (
 	_ "github.com/admpub/translate/providers/baidu"
-	_ "github.com/admpub/translate/providers/google"
+	//_ "github.com/admpub/translate/providers/google"
 	_ "github.com/admpub/translate/providers/libre"
 	_ "github.com/admpub/translate/providers/tencent"
 	_ "github.com/admpub/translate/providers/youdao"
