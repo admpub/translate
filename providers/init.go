08d837444849822675fdaef44e6c2b8d39a0c211
Hank Shen <swhbox@foxmail.com>
2026-04-13 16:11:33 +0800

    update

--- providers/init.go
+++ providers/init.go
@@ -2,8 +2,8 @@ package providers
 
 import (
 	_ "github.com/admpub/translate/providers/baidu"
-	//_ "github.com/admpub/translate/providers/google"
 	_ "github.com/admpub/translate/providers/libre"
+	_ "github.com/admpub/translate/providers/ollama"
 	_ "github.com/admpub/translate/providers/tencent"
 	_ "github.com/admpub/translate/providers/youdao"
 )
